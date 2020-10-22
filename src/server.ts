705e61241dd642928e593ea89ff06f57c5367b1d
Felipe Aranha <felipeparanha@gmail.com>
2020-10-21 23:52:22 -0300

    Adjusts in server.ts file.

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors);
 app.use(express.json());
 
-app.get('/', async (request, response) => {
+app.get('/auth', async (request, response) => {
   const url = 'https://api.instagram.com/oauth/authorize';
   const appId = process.env.INSTAGRAM_APP_ID;
   const redirectURI = process.env.INSTAGRAM_APP_REDIRECT_URI;
@@ -16,7 +16,12 @@ app.get('/', async (request, response) => {
   const fastToken = await axios.get(`${url}?client_id=${appId}&redirect_uri=${redirectURI}&scope=user_profile,user_media&response_type=code`);
 
   console.log(fastToken);
-  return response.json({ message: 'CypherST Server Started' });
+  return response.json({ message: 'Default' });
+});
+
+app.get('/auth', async (request, response) => {
+  console.log(request);
+  return response.json({ message: 'Auth' });
 });
 
 app.listen(process.env.PORT || 3333);
