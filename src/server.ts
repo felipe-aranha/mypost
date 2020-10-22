f043c2aa6f53f96458fd2e0ef3cf8c5c4452f702
Felipe Aranha <felipeparanha@gmail.com>
2020-10-21 23:07:48 -0300

    Install Axios and dotenv.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
+import axios from 'axios';
+import * as dotenv from 'dotenv';
 import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
+dotenv.config();
+
+app.get('/', async (request, response) => {
+  const url = 'https://api.instagram.com/oauth/authorize';
+  const appId = process.env.INSTAGRAM_APP_ID;
+  const redirectURI = process.env.INSTAGRAM_APP_REDIRECT_URI;
+
+  const fastToken = await axios.get(`${url}?client_id=${appId}&redirect_uri=${redirectURI}&scope=user_profile,user_media&response_type=code`);
+
+  console.log(fastToken);
   return response.json({ message: 'CypherST Server Started' });
 });
 
